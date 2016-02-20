01a42fcd969fee5fd3ed67660cd6a6cfd4a9509c
Mod4 <musesc.ll@gmail.com>
2016-02-20 22:56:36 +0900

    itof:sign corrected

--- fpu/itof/itof.vhd
+++ fpu/itof/itof.vhd
@@ -18,78 +18,82 @@ architecture struct of itof is
 	signal flag2: std_logic;
 begin
   sign   <= '0'
-							when input(30 downto 0) = "0000000000000000000000000000000"
+							when input(30 downto 0) = "0000000000000000000000000000000" else
 						input(31);
 
+	sint	 <= input(31 downto 0)
+							when input(31) = '0' else
+						"0" & (input(30 downto 0) xor "0000000000000000000000000000000");
+
 -- shift
-  mant   <= input(29 downto 5) & "1"
-							when input(30) = '1' and input(4 downto 0) /= "00000" else
-						input(28 downto 4) & "1"
-							when input(29) = '1' and input(3 downto 0) /=  "0000" else
-						input(27 downto 3) & "1"
-							when input(28) = '1' and input(2 downto 0) /=   "000" else
-						input(26 downto 2) & "1"
-							when input(27) = '1' and input(1 downto 0) /=    "00" else
-						input(29 downto 4)
-							when input(30) = '1' else
-						input(28 downto 3)
-							when input(29) = '1' else
-						input(27 downto 2)
-							when input(28) = '1' else
-						input(26 downto 1)
-							when input(27) = '1' else
-						input(25 downto 0)
-							when input(26) = '1' else
-						input(24 downto 0) & "0"
-							when input(25) = '1' else
-						input(23 downto 0) & "00"
-							when input(24) = '1' else
-						input(22 downto 0) & "000"
-							when input(23) = '1' else
-						input(21 downto 0) & "0000"
-							when input(22) = '1' else
-						input(20 downto 0) & "00000"
-							when input(21) = '1' else
-						input(19 downto 0) & "000000"
-							when input(20) = '1' else
-						input(18 downto 0) & "0000000"
-							when input(19) = '1' else
-						input(17 downto 0) & "00000000"
-							when input(18) = '1' else
-						input(16 downto 0) & "000000000"
-							when input(17) = '1' else
-						input(15 downto 0) & "0000000000"
-							when input(16) = '1' else
-						input(14 downto 0) & "00000000000"
-							when input(15) = '1' else
-						input(13 downto 0) & "000000000000"
-							when input(14) = '1' else
-						input(12 downto 0) & "0000000000000"
-							when input(13) = '1' else
-						input(11 downto 0) & "00000000000000"
-							when input(12) = '1' else
-						input(10 downto 0) & "000000000000000"
-							when input(11) = '1' else
-						input( 9 downto 0) & "0000000000000000"
-							when input(10) = '1' else
-						input( 8 downto 0) & "00000000000000000"
-							when input( 9) = '1' else
-						input( 7 downto 0) & "000000000000000000"
-							when input( 8) = '1' else
-						input( 6 downto 0) & "0000000000000000000"
-							when input( 7) = '1' else
-						input( 5 downto 0) & "00000000000000000000"
-							when input( 6) = '1' else
-						input( 4 downto 0) & "000000000000000000000"
-							when input( 5) = '1' else
-						input( 3 downto 0) & "0000000000000000000000"
-							when input( 4) = '1' else
-						input( 2 downto 0) & "00000000000000000000000"
-							when input( 3) = '1' else
-						input( 1 downto 0) & "000000000000000000000000"
-							when input( 2) = '1' else
-						input( 0) & "0000000000000000000000000"
-							when input( 1) = '1' else
+  mant   <= sint(29 downto 5) & "1"
+							when sint(30) = '1' and sint(4 downto 0) /= "00000" else
+						sint(28 downto 4) & "1"
+							when sint(29) = '1' and sint(3 downto 0) /=  "0000" else
+						sint(27 downto 3) & "1"
+							when sint(28) = '1' and sint(2 downto 0) /=   "000" else
+						sint(26 downto 2) & "1"
+							when sint(27) = '1' and sint(1 downto 0) /=    "00" else
+						sint(29 downto 4)
+							when sint(30) = '1' else
+						sint(28 downto 3)
+							when sint(29) = '1' else
+						sint(27 downto 2)
+							when sint(28) = '1' else
+						sint(26 downto 1)
+							when sint(27) = '1' else
+						sint(25 downto 0)
+							when sint(26) = '1' else
+						sint(24 downto 0) & "0"
+							when sint(25) = '1' else
+						sint(23 downto 0) & "00"
+							when sint(24) = '1' else
+						sint(22 downto 0) & "000"
+							when sint(23) = '1' else
+						sint(21 downto 0) & "0000"
+							when sint(22) = '1' else
+						sint(20 downto 0) & "00000"
+							when sint(21) = '1' else
+						sint(19 downto 0) & "000000"
+							when sint(20) = '1' else
+						sint(18 downto 0) & "0000000"
+							when sint(19) = '1' else
+						sint(17 downto 0) & "00000000"
+							when sint(18) = '1' else
+						sint(16 downto 0) & "000000000"
+							when sint(17) = '1' else
+						sint(15 downto 0) & "0000000000"
+							when sint(16) = '1' else
+						sint(14 downto 0) & "00000000000"
+							when sint(15) = '1' else
+						sint(13 downto 0) & "000000000000"
+							when sint(14) = '1' else
+						sint(12 downto 0) & "0000000000000"
+							when sint(13) = '1' else
+						sint(11 downto 0) & "00000000000000"
+							when sint(12) = '1' else
+						sint(10 downto 0) & "000000000000000"
+							when sint(11) = '1' else
+						sint( 9 downto 0) & "0000000000000000"
+							when sint(10) = '1' else
+						sint( 8 downto 0) & "00000000000000000"
+							when sint( 9) = '1' else
+						sint( 7 downto 0) & "000000000000000000"
+							when sint( 8) = '1' else
+						sint( 6 downto 0) & "0000000000000000000"
+							when sint( 7) = '1' else
+						sint( 5 downto 0) & "00000000000000000000"
+							when sint( 6) = '1' else
+						sint( 4 downto 0) & "000000000000000000000"
+							when sint( 5) = '1' else
+						sint( 3 downto 0) & "0000000000000000000000"
+							when sint( 4) = '1' else
+						sint( 2 downto 0) & "00000000000000000000000"
+							when sint( 3) = '1' else
+						sint( 1 downto 0) & "000000000000000000000000"
+							when sint( 2) = '1' else
+						sint( 0) & "0000000000000000000000000"
+							when sint( 1) = '1' else
 						"00000000000000000000000000";
 
 	-- round nearest even
@@ -109,67 +113,67 @@ begin
 						mant + 1;
 
 	expo   <= "10011101"
-							when input(30) = '1' or (input(29) = '1' and flag2 = '1') else
+							when sint(30) = '1' or (sint(29) = '1' and flag2 = '1') else
 						"10011100"
-							when input(29) = '1' or (input(28) = '1' and flag2 = '1') else
+							when sint(29) = '1' or (sint(28) = '1' and flag2 = '1') else
 						"10011011"
-							when input(28) = '1' or (input(27) = '1' and flag2 = '1') else
+							when sint(28) = '1' or (sint(27) = '1' and flag2 = '1') else
 						"10011010"
-							when input(27) = '1' or (input(26) = '1' and flag2 = '1') else
+							when sint(27) = '1' or (sint(26) = '1' and flag2 = '1') else
 						"10011001"
-							when input(26) = '1' or (input(25) = '1' and flag2 = '1') else
+							when sint(26) = '1' or (sint(25) = '1' and flag2 = '1') else
 						"10011000"
-							when input(25) = '1' or (input(24) = '1' and flag2 = '1') else
+							when sint(25) = '1' or (sint(24) = '1' and flag2 = '1') else
 						"10010111"
-							when input(24) = '1' else
+							when sint(24) = '1' else
 						"10010110"
-							when input(23) = '1' else
+							when sint(23) = '1' else
 						"10010101"
-							when input(22) = '1' else
+							when sint(22) = '1' else
 						"10010100"
-							when input(21) = '1' else
+							when sint(21) = '1' else
 						"10010011"
-							when input(20) = '1' else
+							when sint(20) = '1' else
 						"10010010"
-							when input(19) = '1' else
+							when sint(19) = '1' else
 						"10010001"
-							when input(18) = '1' else
+							when sint(18) = '1' else
 						"10010000"
-							when input(17) = '1' else
+							when sint(17) = '1' else
 						"10001111"
-							when input(16) = '1' else
+							when sint(16) = '1' else
 						"10001110"
-							when input(15) = '1' else
+							when sint(15) = '1' else
 						"10001101"
-							when input(14) = '1' else
+							when sint(14) = '1' else
 						"10001100"
-							when input(13) = '1' else
+							when sint(13) = '1' else
 						"10001011"
-							when input(12) = '1' else
+							when sint(12) = '1' else
 						"10001010"
-							when input(11) = '1' else
+							when sint(11) = '1' else
 						"10001001"
-							when input(10) = '1' else
+							when sint(10) = '1' else
 						"10001000"
-							when input( 9) = '1' else
+							when sint( 9) = '1' else
 						"10000111"
-							when input( 8) = '1' else
+							when sint( 8) = '1' else
 						"10000110"
-							when input( 7) = '1' else
+							when sint( 7) = '1' else
 						"10000101"
-							when input( 6) = '1' else
+							when sint( 6) = '1' else
 						"10000100"
-							when input( 5) = '1' else
+							when sint( 5) = '1' else
 						"10000011"
-							when input( 4) = '1' else
+							when sint( 4) = '1' else
 						"10000010"
-							when input( 3) = '1' else
+							when sint( 3) = '1' else
 						"10000001"
-							when input( 2) = '1' else
+							when sint( 2) = '1' else
 						"10000000"
-							when input( 1) = '1' else
+							when sint( 1) = '1' else
 						"01111111"
-							when input( 0) = '1' else
+							when sint( 0) = '1' else
 						"00000000";
 	
 	output <= sign & expo & mano;
